073a10e8145e256027cc0114a2b32d9e6ffb644c
4careers <sd4careers@gmail.com>
2026-07-20 12:44:12 +0530

    Add Google Search Console verification tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ import Shortcuts from '../components/Shortcuts';
 export const metadata = {
   title: "Deconstruct | A Modern Philosophy Blog",
   description: "Atheism, Critical Thinking, Secularism, Humanism, Minimalism, Materialism, and Philosophy.",
+  verification: {
+    google: "ju_8BPFau20VshEp8dv6tXIq6Y6f-VDlUe0Gf_uXkPE",
+  },
 };
 
 export default function RootLayout({ children }) {
